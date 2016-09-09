ce0fd819cfba15190ed9c34c70de3a5f332c6db8
javel555 <javel555@gmail.com>
2016-09-10 00:20:00 +0900

    comp

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 /* 3. 以降、アプリ固有処理 */
